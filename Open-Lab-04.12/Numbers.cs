9f0ba74be7015c4c724ce483d4c63cd5baa068d9
Psychoman500 <martin.sutek@gmail.com>
2021-11-19 09:54:31 +0100

    Find number which is not in the array of numbers and return it.

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._12
 {
@@ -6,7 +7,15 @@ namespace Open_Lab_04._12
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            int n = 0;
+            for (int i = 0; i < 11; i++)
+            {
+              if (!nums.Contains(i))
+                {
+                    n = i;
+                }
+            }
+            return n;
         }
     }
 }
